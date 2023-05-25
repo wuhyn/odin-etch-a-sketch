39a847e873bc26cf27940c106f2748c53ad0aa90
Hong Ng <hong.yang.ng@gmail.com>
2023-05-25 21:59:03 +1000

    Implement function to add hover effect

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-
-
-//Select the grid div
-const mainNode = document.querySelector('.grid');
-
+/**
+* Creates a 16x16 Etch A Sketch grid 
+*/
 function createGrid(){
-    // mainNode.parentNode.insertBefore(newNode, mainNode.nextSibling);
+    //Select the grid div
+    const mainNode = document.querySelector('.grid');
+
     for(let i=0; i<16; i++){
         let newNode = document.createElement("div");
 
@@ -15,13 +15,21 @@ function createGrid(){
     }
 }
 
-// Create Etch A Sketch grid
+/**
+* Adds mouseover event listener to each Etch A Sketch grid cell
+*/
+function addHoverEffect(){
+    // Add event listener to trigger hover styling only once
+    const gridCell = document.querySelectorAll(".grid-cell");
+
+    gridCell.forEach(cell => 
+        cell.addEventListener("mouseover", () => cell.classList.toggle("grid-cell-hover"), {once: true})
+    );
+}
+
+
 createGrid();
 
-// Add event listener to trigger hover styling only once
-const gridCell = document.querySelectorAll(".grid-cell");
+addHoverEffect();
 
-gridCell.forEach(cell => 
-    cell.addEventListener("mouseover", () => cell.classList.toggle("grid-cell-hover"), {once: true})
-);
 
