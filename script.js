1b96daf507aeb65d4566b8b43fadf7f90f196e50
Hong Ng <hong.yang.ng@gmail.com>
2023-05-27 09:20:06 +1000

    Update grid, container and item classnames. Switch from use of flexbox to grid

--- script.js
+++ script.js
@@ -2,51 +2,17 @@
 * Creates a 16x16 Etch A Sketch grid 
 */
 function createGrid(){
-    //Select the grid div
+
     const mainNode = document.querySelector('.grid');
 
-    let gridCell;
+    // let gridCell;
 
     for(let i=0; i<16; i++){
         let newNode = document.createElement("div");
 
-        newNode.classList.toggle("grid-cell");
+        newNode.classList.toggle("grid-item");
 
         mainNode.appendChild(newNode);
-    }
-
-    gridCell = document.querySelectorAll('.grid-cell');
-
-    for(let i = 0; i < gridCell.length; i++){
-        gridCell[i].style.width = "103px";
-        gridCell[i].style.height = "100px";
-    }
-}
-
-function updateGrid(gridSize){
-    //Select the grid div
-    const mainNode = document.querySelector('.grid');
-    let gridCell;
-    let width;
-    let height;
-
-    while (mainNode.firstChild) {
-        mainNode.removeChild(mainNode.lastChild);
-    }
-
-    for(let i=0; i<gridSize; i++){
-        let newNode = document.createElement("div");
-
-        newNode.classList.toggle("grid-cell");
-
-        mainNode.appendChild(newNode);
-    }
-
-    gridCell = document.querySelectorAll('.grid-cell');
-
-    for(let i = 0; i < gridCell.length; i++){
-        gridCell[i].style.width = width;
-        gridCell[i].style.height = width;
     }
 }
 
@@ -65,26 +31,3 @@ function addHoverEffect(){
 createGrid();
 
 addHoverEffect();
-
-/**
-* Add listener
-*/
-const promptButton = document.querySelector(".prompt-btn");
-
-promptButton.addEventListener("click", function(){
-    const promptGridSize = prompt("Please enter a new Etch A Sketch grid size.");
-
-    const gridSize = parseInt(promptGridSize);
-
-    if(isNaN(gridSize)){
-        console.log("Please enter a number.")
-    }
-
-    else if(gridSize > 100){
-        console.log("This is not a valid number. Please select a number less than 100");
-    }
-
-    else {
-        updateGrid(gridSize);
-    }
-})
