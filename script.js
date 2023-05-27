ab351ae37122d63fdd7244e06a77bcdebc37dd36
Hong Ng <hong.yang.ng@gmail.com>
2023-05-27 11:17:58 +1000

    Add prompt function to update grid based on user input

--- script.js
+++ script.js
@@ -5,15 +5,43 @@ function createGrid(){
 
     const mainNode = document.querySelector('.grid');
 
-    // let gridCell;
-
     for(let i=0; i<16; i++){
+        
         let newNode = document.createElement("div");
 
         newNode.classList.toggle("grid-item");
 
         mainNode.appendChild(newNode);
     }
+}
+
+/**
+* Creates a dynamic Etch A Sketch grid 
+*/
+function updateGrid(gridSize){
+    const mainNode = document.querySelector('.grid');
+
+    // Remove all child elements
+    while (mainNode.firstChild) {
+        mainNode.removeChild(mainNode.lastChild);
+    }
+
+    // Generate new grid items
+    for(let i=0; i<gridSize; i++){
+        let newNode = document.createElement("div");
+
+        newNode.classList.toggle("grid-item");
+
+        mainNode.appendChild(newNode);
+    }
+
+    //Dynamically set the grid template size for rows and columns based on user input
+    const grid = document.querySelector('.grid');
+
+    let gridTemplateSize = Math.sqrt(gridSize);
+
+    grid.setAttribute("style", "background-color: blue;");
+    grid.setAttribute("style", `grid-template-columns: repeat(${gridTemplateSize}, 1fr); grid-template-rows: repeat(${gridTemplateSize}, 1fr);`);
 }
 
 /**
@@ -31,3 +59,26 @@ function addHoverEffect(){
 createGrid();
 
 addHoverEffect();
+
+/**
+* Add listener
+*/
+const promptButton = document.querySelector(".prompt-btn");
+
+promptButton.addEventListener("click", function(){
+    const promptGridSize = prompt("Please enter a new Etch A Sketch grid size.");
+
+    const gridSize = parseInt(promptGridSize);
+
+    if(isNaN(gridSize)){
+        console.log("Please enter a number.")
+    }
+
+    else if(gridSize > 100){
+        console.log("This is not a valid number. Please select a number less than 100");
+    }
+
+    else {
+        updateGrid(gridSize);
+    }
+})
